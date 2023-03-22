48858bd3706e606c7a0acfb1129cc6966d323d9b
PabloGarciaCaldero <pablo-.maria.garcia@estudiantat.upc.edu>
2023-03-22 11:31:49 +0100

    conexion al backend de sem4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { Routes, RouterModule } from '@angular/router';
 import { Employee } from 'src/models/employee';
 import { ConnectionStates } from 'mongoose';
+import { User } from 'src/models/user';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,20 @@ import { ConnectionStates } from 'mongoose';
 })
 
 export class AppComponent {
+  
 
 
   constructor(private _employeeService: EmployeeService) {}
 
   ngOnInit(): void {
-    this.obtenerEmployees();
+    this.obtenerUsers();
+    //this.obtenerEmployees();
   }
 
-  obtenerEmployees(){
-    this._employeeService.getEmployees().subscribe(data => {
+  obtenerUsers(){
+    this._employeeService.getUsers().subscribe(data => {
       console.log(data);
-      this.employees = data;
+      this.users = data;
 /*      for(let i=0; i < data.length; i++){
         this.employees.push(data[i]);
       }*/
@@ -32,45 +35,45 @@ export class AppComponent {
     })
   }
 
-  eliminarEmployee(id:string){
+  eliminarUser(id:string){
     var answer = confirm('Estas seguro de querer eliminarlo?');
     if(answer){
-      this._employeeService.eliminarEmployee(id).subscribe(data => {
-        this.employees = [];
-        this.obtenerEmployees();    
+      this._employeeService.eliminarUser(id).subscribe(data => {
+        this.users = [];
+        this.obtenerUsers();    
       }, error => {
         console.log(error);
       })
     }    
   }
-  deleteAllEmployees(){
+  deleteAllUsers(){
     var answer = confirm('Estas seguro de querer eliminar todos los usuarios?');
     if(answer){
-      this._employeeService.deleteTodosEmployees().subscribe(data => {
-        this.employees = [];
-        this.obtenerEmployees();    
+      this._employeeService.deleteTodosUsers().subscribe(data => {
+        this.users = [];
+        this.obtenerUsers();    
       }, error => {
         console.log(error);
       })
     }
   }
 
-  agregarEmployee(){
-    this._employeeService.añadirEmployee(this.model).subscribe(data => {
-      this.employees = [];
-      this.obtenerEmployees();
-      this.model = {_id:'',name:'',position:'',office:'',salary:0};  
+  agregarUser(){
+    this._employeeService.añadirUser(this.model).subscribe(data => {
+      this.users = [];
+      this.obtenerUsers();
+      this.model = {_id:'',name:'',surname:'',email:'',password:0};  
     }, error => {
       console.log(error);
     })
   }
 
-  editarEmployee(id:string){
-    this._employeeService.actualizarEmployee(id,this.model2).subscribe(data =>{
-      this.model2 = {_id:'',name:'',position:'',office:'',salary:0};
+  editarUser(id:any){
+    this._employeeService.actualizarUser(id,this.model2).subscribe(data =>{
+      this.model2 = {_id:'',name:'',surname:'',email:'',password:0};
       this.hideUpdate = true;
-      this.employees = [];
-      this.obtenerEmployees();
+      this.users = [];
+      this.obtenerUsers();
     }, error => {
       console.log(error);
     })
@@ -81,22 +84,22 @@ export class AppComponent {
 
   title:string = 'Angular CRUD';  
 
-  employees: Employee [] = [];
+  users: User [] = [];
 
-  model:Employee = {_id:'',name:'',position:'',office:'',salary:0};
-  model2:Employee = {_id:'',name:'',position:'',office:'',salary:0};
+  model:User = {_id:'',name:'',surname:'',email:'',password:0};
+  model2:User = {_id:'',name:'',surname:'',email:'',password:0};
   msg:string = '';
   hideUpdate:boolean = true;
 
-  myValue = 0;
-  editEmployee(i:number):void{
-    this.hideUpdate = false;
-    this.model2._id = this.employees[i]._id;
-    this.model2.name = this.employees[i].name;
-    this.model2.position = this.employees[i].position;
-    this.model2.office = this.employees[i].office;
-    this.myValue = i;
-  }
+  // myValue = 0;
+  // editEmployee(i:number):void{
+  //   this.hideUpdate = false;
+  //   this.model2._id = this.users[i]._id;
+  //   this.model2.name = this.employees[i].name;
+  //   this.model2.position = this.employees[i].position;
+  //   this.model2.office = this.employees[i].office;
+  //   this.myValue = i;
+  // }
   
   closeAlert():void{
     this.msg = '';
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -3,33 +3,57 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 // import { Employee } from '../app.component';
 import { Employee } from 'src/models/employee';
+import { User } from 'src/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  url = 'http://localhost:3000/api/employees';
+  // url = 'http://localhost:3000/api/employees';
 
+  // constructor(private http: HttpClient) { }
+
+  // getEmployees(): Observable<Employee[]> {
+  //   return this.http.get<Employee[]>(this.url);
+  // }
+
+  // eliminarEmployee(id: String): Observable<any> {
+  //   return this.http.delete(this.url + '/' + id);
+  // } 
+
+  // añadirEmployee(employee: Employee): Observable<any>{
+  //   return this.http.post(this.url, employee);
+  // }
+
+  // actualizarEmployee(id:String, employee: Employee): Observable<any>{
+  //   return this.http.put(this.url + '/' + id, employee);
+  // }
+
+  // deleteTodosEmployees(): Observable<any>{
+  //   return this.http.delete(this.url + '/');
+  // }
+
+  url = 'http://localhost:3000/api/employees'
   constructor(private http: HttpClient) { }
 
-  getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(this.url);
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.url + '/all');
   }
 
-  eliminarEmployee(id: String): Observable<any> {
+  eliminarUser(id: String): Observable<any> {
     return this.http.delete(this.url + '/' + id);
   } 
 
-  añadirEmployee(employee: Employee): Observable<any>{
-    return this.http.post(this.url, employee);
-  }
-
-  actualizarEmployee(id:String, employee: Employee): Observable<any>{
-    return this.http.put(this.url + '/' + id, employee);
-  }
-
-  deleteTodosEmployees(): Observable<any>{
+  deleteTodosUsers(): Observable<any> {
     return this.http.delete(this.url + '/');
+  } 
+
+  añadirUser(user: User): Observable<any>{
+    return this.http.post(this.url + '/', user);
+  }
+
+  actualizarUser(id:String, user: User): Observable<any>{
+    return this.http.put(this.url + '/' + id, user);
   }
 
 }
